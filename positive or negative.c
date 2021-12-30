8d234262446193c2d1c27ea4b958d519794e145b
vkmanoj <96334448+vkmanoj@users.noreply.github.com>
2021-12-30 15:41:57 +0530

    Add files via upload

---
+++ positive or negative.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main()
+{
+	int num;
+	printf("enter the number");
+	scanf("%d",&num);
+	if(num>0)
+	{
+		printf("given number is positive");
+	}
+	else
+	{
+		printf("given number is negative");
+	}
+}
